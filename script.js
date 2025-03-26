c29858c2248de46f44702406699ed6110cd9f709
Maksim <contact@maksim.fit>
2025-03-26 22:32:56 +0300

    add some fixes

--- script.js
+++ script.js
@@ -12,8 +12,12 @@ const generateRandomQuote = () => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
   $("quote").textContent = randomQuote;
+  // если нет return то функция по умолчанию возвращает undefined
 }
 
 $("generate-btn").addEventListener('click', generateRandomQuote);
 
 generateRandomQuote();
+
+// параллелепипед - свойство 
+// куб - метод
